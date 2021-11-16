f7653b1c5656176fb49c08223d3c63609db2c74d
Steffany Cristina <scro13@hotmail.com>
2021-11-15 22:54:46 -0300

    Checkpoint

---
+++ Aula 10/Mesa.js
@@ -0,0 +1,16 @@
+let numbers =[22, 33, 54, 66, 72]
+console.log(numbers[numbers.length])
+
+
+let grupoDeAmigos = [ ["Harry", "Ron", "Hermione"], ["Spiderman", "Hulk", "Ironman"], ["Penélope Glamour", "Pierre Nodoyuna","Patán"] ]
+console.log(grupoDeAmigos[1][0])
+
+
+let str = “una string qualquer”
+let grupoDeAmigos = [ [45, 89, 0], ["Digital", "House", true], ["string", "123","false", 54, true, str] ]
+console.log(grupoDeAmigos[2][grupoDeAmigos[2].length - 1])
+
+
+somarArray([1,2,3])                // 6
+somarArray([10, 3, 10, 4])     // 27
+somarArray([-5,100])             // 95
